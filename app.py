c84a46272e034ba3872263978d8c981a63049526
sheyn018 <sheane39@gmail.com>
2024-06-18 05:47:42 -0400

    fixed html to load chatbot

--- app.py
+++ app.py
@@ -130,42 +130,42 @@ def main():
             st.sidebar.success("Documents processed and vectors upserted successfully!")
     
     st.header("Chatbot")
-    chatbot_html = """
+    chatbot_html = f"""
     <div class="chatbot-inner section-inner">
         <div id="flat-chat"></div>
         <style>
-            #flat-chat {
+            #flat-chat {{
                 width: 100%;
                 height: 800px; /* Adjust the height as needed */
                 border: none;
                 overflow: hidden;
-            }
+            }}
         </style>
         <script type="text/javascript">
-            (function(d, t) {
+            (function(d, t) {{
                 var v = d.createElement(t),
                     s = d.getElementsByTagName(t)[0];
-                v.onload = function() {
-                    window.voiceflow.chat.load({
-                        verify: {
+                v.onload = function() {{
+                    window.voiceflow.chat.load({{
+                        verify: {{
                             projectID: '{project_id}'
-                        }, // Replace with your Voiceflow project ID
+                        }},
                         url: 'https://general-runtime.voiceflow.com',
                         versionID: 'production',
-                        render: {
+                        render: {{
                             mode: 'embedded',
                             target: document.getElementById('flat-chat'),
-                        },
+                        }},
                         autostart: false,
-                    });
-                }
+                    }});
+                }};
                 v.src = "https://cdn.voiceflow.com/widget/bundle.mjs";
-                v.type = "text/javascript";
                 s.parentNode.insertBefore(v, s);
-            })(document, 'script');
+            }})(document, 'script');
         </script>
     </div>
     """
+
     components.html(chatbot_html, height=850)
 
 if __name__ == '__main__':
